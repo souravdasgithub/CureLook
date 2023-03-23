ab1a06b71de4e4a709c34fd000a30a9472d34dab
SOURAV DAS <128668163+souravdasgithub@users.noreply.github.com>
2023-03-23 10:16:12 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+$address= $_POST['address'];
+$docname= $_POST['docname'];
+$departments= $_POST['departments'];
+$date= $_POST['date'];
+
+$to = "souravinbox.sd@gmail.com";
+$subject = "Booking from CureLook";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone No. =" . $phone . "\r\n Address =" . $address . "\r\n Doctor's Name =" . $docname . "\r\n Departments =" . $departments . "\r\n Date of Booking =" . $date;
+$headers = "From: bookfrom@curelook.com" . "\r\n" .
+"CC: jordonhardy6020@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:confirm_booking.html");
+?>
